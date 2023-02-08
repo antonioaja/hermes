9fc82f0b91fcfa8c4dec6b8ceeb0228b14a1dcf9
Antonio Aguilar <antonioaja@protonmail.com>
2023-02-08 10:59:04 -0500

    Reformatted

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,44 @@ use clap::Parser;
 use std::convert::TryFrom;
 use suppaftp::{list, FtpStream};
 
+fn main() -> Result<()> {
+    // Parse CLI arguments
+    let args: Args = Args::parse();
+
+    // Connect to the server
+    let mut stream = FtpStream::connect(format!("{}:{}", &args.ip, &args.port)).context(
+        format!("Could not connect to {} on port {}", &args.ip, &args.port),
+    )?;
+    stream.login(&args.user, &args.password).context(format!(
+        "Could not login with these credentials:\nUser: {}\nPassword: {}",
+        &args.user, &args.password
+    ))?;
+
+    // Change into target directory
+    stream
+        .cwd(&args.folder)
+        .context(format!("Could not change directory into {}", &args.folder))?;
+
+    // Parse name of files
+    let files: Vec<list::File> = stream
+        .list(None)
+        .ok()
+        .context(format!(
+            "Could not list files in {:?}",
+            stream
+                .pwd()
+                .context("Could not return the current directory")?
+        ))?
+        .iter()
+        .map(|x| list::File::try_from(x.as_str()).ok().unwrap())
+        .collect();
+    for i in files {
+        println!("{}", i.name());
+    }
+
+    Ok(())
+}
+
 #[derive(Parser, Debug)]
 #[clap(
     author = "Antonio Aguilar",
@@ -41,42 +79,4 @@ struct Args {
     /// Password of the server
     #[clap(short, long, value_parser, default_value = "")]
     password: String,
-}
-
-fn main() -> Result<()> {
-    // Parse CLI arguments
-    let args: Args = Args::parse();
-
-    // Connect to the server
-    let mut stream = FtpStream::connect(format!("{}:{}", &args.ip, &args.port)).context(
-        format!("Could not connect to {} on port {}", &args.ip, &args.port),
-    )?;
-    stream.login(&args.user, &args.password).context(format!(
-        "Could not login with these credentials:\nUser: {}\nPassword: {}",
-        &args.user, &args.password
-    ))?;
-
-    // Change into target directory
-    stream
-        .cwd(&args.folder)
-        .context(format!("Could not change directory into {}", &args.folder))?;
-
-    // Parse name of files
-    let files: Vec<list::File> = stream
-        .list(None)
-        .ok()
-        .context(format!(
-            "Could not list files in {:?}",
-            stream
-                .pwd()
-                .context("Could not return the current directory")?
-        ))?
-        .iter()
-        .map(|x| list::File::try_from(x.as_str()).ok().unwrap())
-        .collect();
-    for i in files {
-        println!("{}", i.name());
-    }
-
-    Ok(())
 }
